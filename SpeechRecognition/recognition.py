c871c02b61772713f180882989ae0f214688f890
twrdyyy <filip.twardy.v01@gmail.com>
2018-11-01 18:11:03 +0100

    Speech Recognition

---
+++ SpeechRecognition/recognition.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+if __name__ == "__main__":
+
+    import speech_recognition as sr
+
+    # Record Audio
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source, phrase_time_limit=4)
+
+    # Speech recognition using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        print("You said: " + r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
